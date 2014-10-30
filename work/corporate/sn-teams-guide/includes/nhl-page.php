f1fed4a88449ab92faa7c7060f5699ec2a71fb87
dpitchford1 <pitchford.dylan@gmail.com>
2014-10-30 17:45:07 -0400

    Templating Teams and Leagues
    - Added teams list
    - added leagues listing
    - added leagues includes
    - added nhl variables array files

---
+++ work/corporate/sn-teams-guide/includes/nhl-page.php
@@ -0,0 +1,79 @@
+<?php include "nhl-vars.php"; ?>
+
+<?php /*
+	echo "<pre>";
+	print_r($nhl);
+	echo "</pre>";
+*/ ?>
+
+<h2>Choose a Team:</h2>
+<form>
+	<select onChange="if(this.selectedIndex!=0) self.location=this.options[this.selectedIndex].value">
+		<option value="" selected>Select a Team</option>
+	<?php foreach ($nhl as $teams => $values) { ?>
+		<option value="#<?=$values['LongName']?>"><?=$values['FullName']?></option>
+	<?php } ?>
+	</select>
+</form>
+
+<?php /** START TEAM */ ?>
+
+<?php foreach ($nhl as $teams => $values) { ?>
+
+<h3 class="sg-subheading" style="color: <?=$values['HeadingColour']?>" id="<?=$values['LongName']?>"><?=$values['FullName']?></h3>
+
+<h3 class="sg-tileheading">Team Colours</h3>
+
+<ul class="sg-colors">
+	<?php foreach ($values['TeamColours'] as $colors => $color_value) {	?>
+	<li>
+		<span style="background: <?=$color_value?>" class="sg-swatch">&nbsp;</span>
+		<p class="sg-label"><?=$color_value?></p>
+	</li>
+	<?php }	?>
+</ul>
+
+<p class="ico-inline ico-25-inline <?=$values['ShortName']?>--25x25">Some text to show bg on a paragraph</p>
+<p class="ico-inline ico-25-inline <?=$values['ShortName']?>--25x25"><a href="">Text to show bg on a paragraph with a link</a></p>
+
+<h4 class="sg-tileheading">Logos - Inline &mdash; with BG color</h4>
+<ul class="sg-logos <?=$values['ShortName']?>">
+	<li class="logo-59">
+		<img src="img/team_logos/59x59/hockey/nhl/<?=$values['LongName']?>.png" alt="">
+		<p class="sg-label">59x59</p>
+	</li>
+	<li class="logo-90">
+		<img src="img/team_logos/90x90/hockey/nhl/<?=$values['LongName']?>.png" alt="">
+		<p class="sg-label">90x90</p>
+	</li>
+	<li class="logo-170">
+		<img class="bgcolor" src="img/team_logos/170x170/hockey/nhl/<?=$values['LongName']?>.png" alt="">
+		<p class="sg-label">170x170</p>
+	</li>
+	<li class="logo-200">
+		<img src="img/team_logos/200x200/hockey/nhl/<?=$values['LongName']?>.png" alt="">
+		<p class="sg-label">200x200</p>
+	</li>
+</ul>
+
+<h4 class="sg-tileheading">Logos - As Backgrounds &mdash; with BG color</h4>
+<ul class="sg-logos <?=$values['ShortName']?>">
+	<li class="logo-59">
+		<span class="ico-59 ico <?=$values['ShortName']?>--59x59"></span>
+		<p class="sg-label">59x59</p>
+	</li>
+	<li class="logo-90">
+		<span class="ico-90 ico <?=$values['ShortName']?>--90x90"></span>
+		<p class="sg-label">90x90</p>
+	</li>
+	<li class="logo-170">
+		<span class="ico-170 ico <?=$values['ShortName']?>--170x170"></span>
+		<p class="sg-label">170x170</p>
+	</li>
+	<li class="logo-200">
+		<span class="bgcolor ico-200 ico <?=$values['ShortName']?>--200x200"></span>
+		<p class="sg-label">200x200</p>
+	</li>
+</ul>
+
+<?php } ?>
---
+++ work/corporate/sn-teams-guide/includes/nhl-vars.php
@@ -0,0 +1,138 @@
+<?php
+
+	$nhl = array(
+
+		"anh" => array(
+			"FullName" => "Anaheim Ducks",
+			"LongName" => "anaheim-ducks",
+			"ShortName" => "nhl-anh",
+			"HeadingColour" => "#91764B",
+			"TeamColours" => array(
+				colour1 => "#000",
+				colour2 => "#91764B",
+				colour3 => "#EF5225"
+				)
+			),
+
+		"bos" => array(
+			"FullName" => "Boston Bruins",
+			"LongName" => "boston-bruins",
+			"ShortName" => "nhl-bos",
+			"HeadingColour" => "#000",
+			"TeamColours" => array(
+				colour1 => "#000",
+				colour2 => "#FFC422",
+				colour3 => "#fff"
+				)		
+			),
+
+	);
+
+?>
+
+<?php /*
+
+Full Names
+
+Anaheim Ducks
+Boston Bruins
+Buffalo Sabres
+Calgary Flames
+Carolina Hurricanes
+Chicago Blackhawks
+Colorado Avalanche
+Columbus Blue Jackets
+Dallas Stars
+Detroit Red Wings
+Edmonton Oilers
+Florida Panthers
+Los Angeles Kings
+Minnesota Wild
+Montreal Canadiens
+Nashville Predators
+New Jersey Devils
+New York Islanders
+New York Rangers
+Ottawa Senators
+Philadelphia Flyers
+Phoenix Coyotes
+Pittsburgh Penguins
+San Jose Sharks
+St. Louis Blues
+Tampa Bay Lightning
+Toronto Maple Leafs
+Vancouver Canucks
+Washington Capitals
+Winnipeg Jets
+
+
+
+Long Names
+
+anaheim-ducks
+boston-bruins
+buffalo-sabres
+calgary-flames
+carolina-hurricanes
+chicago-blackhawks
+colorado-avalanche
+columbus-blue-jackets
+dallas-stars
+detroit-redwings
+edmonton-oilers
+florida-panthers
+los-angeles-kings
+minnesota-wild
+montreal-canadiens
+nashville-predators
+new-jersey-devils
+new-york-islanders
+new-york-rangers
+ottawa-senators
+philadelphia-flyers
+phoenix-coyotes
+pittsburgh-penguins
+san-jose-sharks
+st-louis-blues
+tampa-bay-lightning
+toronto-maple-leafs
+vancouver-canucks
+washington-capitals
+winnipeg-jets
+
+
+Short Names
+
+nhl-anh
+nhl-bos
+nhl-buf
+nhl-cgy
+nhl-car
+nhl-chi
+nhl-col
+nhl-cls
+nhl-dal
+nhl-det
+nhl-edm
+nhl-fla
+nhl-la
+nhl-min
+nhl-mon
+nhl-nsh
+nhl-nj
+nhl-nyi
+nhl-nyr
+nhl-ott
+nhl-phi
+nhl-pho
+nhl-pit
+nhl-sj
+nhl-stl
+nhl-tb
+nhl-tor
+nhl-van
+nhl-was
+nhl-wpg
+*/ ?>
+
+
